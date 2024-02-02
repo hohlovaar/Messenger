5fc6c87a944637416841b96978de405a93e4941f
PopovVAPV <popov.va.di11@mail.ru>
2024-02-02 14:39:28 +0300

    #1задача-создали UI окна входа и окна чата

--- Messenger/MainWindow.xaml.cs
+++ Messenger/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,45 @@ namespace Messenger
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static Chat ChatWindow;
+        static SqlConnection conn;
         public MainWindow()
         {
             InitializeComponent();
+            conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Users;
+            Integrated Security=True;");
+        }
+
+        private void Button_Enter(object sender, RoutedEventArgs e)
+        {
+            string login = loginBox.Text;
+            string password = passBox.Password;
+
+
+            string query = "SELECT COUNT(*) FROM Users WHERE Login = @login AND Password = @password";
+
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.Add("@login", SqlDbType.NVarChar, 10).Value = login;
+                cmd.Parameters.Add("@password", SqlDbType.NVarChar, 10).Value = password;
+
+                conn.Open();
+                int count = (int)cmd.ExecuteScalar();
+                conn.Close();
+
+                if (count > 0)
+                {
+                    ChatWindow = new Chat();
+                    ChatWindow.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Неправильный логин или пароль");
+                }
+            }
         }
     }
 }
+
+  
