2eab3d49ed4a9e6de520d94007510556c05c51cd
PopovVAPV <popov.va.di11@mail.ru>
2024-02-02 15:30:19 +0300

    #2

--- Messenger/Chat.xaml.cs
+++ Messenger/Chat.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,10 +21,21 @@ namespace Messenger
     /// </summary>
     public partial class Chat : Window
     {
+        SqlCommand sqlCommand;
+        //User user;
         public static MainWindow Window;
         public Chat()
         {
             InitializeComponent();
+            //this.user = user;
+        }
+
+        private void Button_Send(object sender, RoutedEventArgs e)
+        {
+            string query = $"INSERT INTO MESSAG VALUES ('{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}',{(user.id == 1 ? 2:1)}, {user.id}, '{TextMessageBox.Text}')";
+            sqlCommand = new SqlCommand(query, MainWindow.conn);
+            sqlCommand.ExecuteNonQuery();
+            TextMessageBox.Text = "";
         }
 
         private void Button_Exit(object sender, RoutedEventArgs e)
@@ -31,7 +44,5 @@ namespace Messenger
             Window.Show();
             this.Close();
         }
-
-        
     }
 }
